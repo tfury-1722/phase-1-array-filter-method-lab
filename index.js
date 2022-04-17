afa5436e771efaae32f0f14a7810d55ac574047c
christopher <cdnance1829@gmail.com>
2022-04-17 16:04:46 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+function findMatching(driverNames, name) {
+    for (const entry of driverNames) {
+        const final = driverNames.filter(function (entry) {
+            if (entry.toLowerCase() === name.toLowerCase()) {
+                return entry;
+            }
+
+        })
+        return final;
+    }
+}
+
+function fuzzyMatch(driverNames, query) {
+    for (const member of driverNames) {
+        const fuzz = driverNames.filter(member => member.startsWith(query));
+        return fuzz;
+    }
+    return fuzz;
+}
+
+function matchName(objArray, name) {
+    for (const item of objArray) {
+        const id = objArray.filter(item => item.name === name);
+        return id;
+    }
+    return id;
+}
